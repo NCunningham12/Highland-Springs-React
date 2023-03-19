7aff24b02dd66154351d764a1d75a0c2efbd33c1
Nathan Cunningham <counterweight22@gmail.com>
2023-03-18 20:03:25 -0700

    Hero Section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+import bgVideo from '../videos/bgvideo.mp4'
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src={bgVideo} className="bg-video" type='video/mp4' autoPlay loop muted />
+      <h1>HIGHLAND SPRINGS <br /> GOLF COMMUNITY <hr /></h1> 
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET INFO
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH PROMO <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
